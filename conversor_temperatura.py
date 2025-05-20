d83284dad5c69592ae1134076ffe18710d211020
baileytorch <baileytorch@hotmail.com>
2025-05-20 11:54:42 -0400

    Conversor temperatura

---
+++ conversor_temperatura.py
@@ -0,0 +1,34 @@
+# Sistema de conversión de temperatura entre distintas escalas, Celsius, Fehrenheit y Kelvin.
+# 1.- Construir Menu
+# 2.- Definir funciones de conversión
+# 3.- Pedir datos al usuario
+# 4.- Realizar conversión
+# 5.- Mostrar Resultado
+
+# ºC K = T+273.15	
+# °C °F = (T*1.8)+32
+# K °C = T-273.15
+# K °F = (T-273.15)*9/5+32
+# ºF °C = (T-32)/1.8	
+# °F K = (T-32)*5/9+273.15
+
+def cargar_menu():
+    print("[1] Convertir °C a K.")
+    print("[2] Convertir °C a °F.")
+    print("[3] Convertir K a °C.")
+    print("[4] Convertir K a °F.")
+    print("[5] Convertir °F a °C.")
+    print("[6] Convertir °F a K.")
+    print("[0] Salir.")
+    
+def convertir_celsius_kelvin(temperatura_inicial):
+    temperatura = temperatura_inicial + 273.15
+    return temperatura
+
+def convertir_celsius_fahrenheit(temperatura_inicial):
+    temperatura = (temperatura_inicial * 1.8) + 32
+    return temperatura
+
+def convertir_kelvin_celsius(temperatura_inicial):
+    temperatura = temperatura_inicial - 273.15
+    return temperatura

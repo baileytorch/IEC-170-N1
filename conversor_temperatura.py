c552d34679c1eb48ecab1888beb02343a06d147a
baileytorch <baileytorch@hotmail.com>
2025-05-20 12:24:02 -0400

    Actualización conversor temperatura

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -10,7 +10,7 @@
 # K °C = T-273.15
 # K °F = (T-273.15)*9/5+32
 # ºF °C = (T-32)/1.8	
-# °F K = (T-32)*5/9+273.15
+# °F K = (T-32)+273.15
 
 def cargar_menu():
     print("[1] Convertir °C a K.")
@@ -32,3 +32,44 @@ def convertir_celsius_fahrenheit(temperatura_inicial):
 def convertir_kelvin_celsius(temperatura_inicial):
     temperatura = temperatura_inicial - 273.15
     return temperatura
+
+def convertir_kelvin_fahrenheit(temperatura_inicial):
+    temperatura = convertir_kelvin_celsius(temperatura_inicial) * 9/5 + 32
+    return temperatura
+
+def convertir_fahrenheit_celsius(temperatura_inicial):
+    temperatura = (temperatura_inicial - 32) / 1.8
+    return temperatura
+
+def convertir_fahrenheit_kelvin(temperatura_inicial):
+    temperatura = convertir_fahrenheit_celsius(temperatura_inicial) + 273.15
+    return temperatura
+
+def programa_principal():
+    print()
+    print("Súper Conversor de Temperaturas!!")
+    print("=================================")
+    
+    while True:
+        cargar_menu()
+        opcion = input("Selecciones su opción [0-6]")
+        temperatura_usuario = input("Ingrese su Temperatura Inicial: ")
+        
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            pass
+        elif opcion == "6":
+            pass
+        elif opcion == "0":
+            print("Chao chao loh vimoh!")
+            break
+        else:
+            print("Opción Inválida")
+            return
